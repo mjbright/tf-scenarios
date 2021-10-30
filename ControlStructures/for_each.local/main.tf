dde13252a91ac379a1d8fc28ab8cad2fe0d63b78
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-10-30 19:52:28 +0000

    Added for_each.local example

---
+++ ControlStructures/for_each.local/main.tf
@@ -0,0 +1,15 @@
+
+variable opfiles {
+    type    = list(string)
+    default = [ "file1.txt", "file2.txt", "file3.txt", "file4.txt" ]
+}
+
+# Using local_file in-built Provider for light weight example
+resource "local_file" "foo" {
+    for_each  = toset(var.opfiles)
+
+    # Note: key is same as value:
+    content  = "File[${each.key}]: Content for ${each.value}\n"
+    filename = "${path.module}/files/${each.value}"
+}
+
