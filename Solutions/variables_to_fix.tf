f45cd63bf64c522669936caed5a48dcac0589739
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-11-01 08:43:03 +0000

    Added 'to_fix' example and solution for variable types

---
+++ Solutions/variables_to_fix.tf
@@ -0,0 +1,61 @@
+
+variable t1 {
+    type = number
+    default = 111
+}
+
+variable t2 {
+    type = string
+    default = "111"
+}
+
+variable t3 {
+    type = bool
+    default = true
+}
+
+variable t4 {
+    type = list(string)
+    default = [ "1", "2", "3" ]
+}
+
+variable t5 {
+    type = list(number)
+    default = [ 1, 2, 3 ]
+}
+
+variable t6 {
+    type = list(bool)
+    default = [ true, false, true ]
+}
+
+# All values coerced to string:
+variable t7 {
+    type = map(string)
+    default = { "a": 1, "b": true, "c": "3" }
+}
+
+# All values coerced to string:
+variable t7b {
+    type = map(any)
+    default = { "a": 21, "b": true, "c": "23" }
+}
+
+variable t8 {
+    type = number
+    default = 1000.0 * 10
+}
+
+variable t9 {
+    type = number
+    default = 10 / 10
+}
+
+variable t10 {
+    #type = X
+    #type = map(any)
+    #type = object(map)
+    default = { "a": "hello", "b": { "a": "hello", "b": 1 }, "c": "bye" }
+}
+
+
