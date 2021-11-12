065e38bc4e0d2739a866899416be7137d052b79c
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-11-12 14:32:03 +0000

    Added colour definitions

--- Dependencies/depends_on.local/archive_1file/run_demo.sh
+++ Dependencies/depends_on.local/archive_1file/run_demo.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
+# TODO: cat file with highlights
+# TODO: 
+
 cd $(dirname $0)
+
+
+## -- Colours: -----------------------------------------------
+
+#  NORMAL;           BOLD;                 INVERSE;
+_black='\e[00;30m';  _L_black='\e[01;30m';  _I_black='\e[07;30m'
+_white='\e[00;37m';  _L_white='\e[01;37m';  _I_white='\e[07;37m'
+_red='\e[00;31m';    _L_red='\e[01;31m';    _I_red='\e[07;31m'
+_green='\e[00;32m';  _L_green='\e[01;32m'   _I_green='\e[07;32m'
+_yellow='\e[00;33m'; _L_yellow='\e[01;33m'  _I_yellow='\e[07;33m'
+_blue='\e[00;34m'    _L_blue='\e[01;34m'    _I_blue='\e[07;34m'
+_magenta='\e[00;35m' _L_magenta='\e[01;35m' _I_magenta='\e[07;35m'
+_cyan='\e[00;36m'    _L_cyan='\e[01;36m'    _I_cyan='\e[07;36m'
+
+_norm='\e[00m'
+
+black()   { echo -en $_black;   echo -n "$*" ; echo -en $_norm; }
+white()   { echo -en $_white;   echo -n "$*" ; echo -en $_norm; }
+red()     { echo -en $_red;     echo -n "$*" ; echo -en $_norm; }
+green()   { echo -en $_green;   echo -n "$*" ; echo -en $_norm; }
+yellow()  { echo -en $_yellow;  echo -n "$*" ; echo -en $_norm; }
+blue()    { echo -en $_blue;    echo -n "$*" ; echo -en $_norm; }
+magenta() { echo -en $_magenta; echo -n "$*" ; echo -en $_norm; }
+cyan()    { echo -en $_cyan;    echo -n "$*" ; echo -en $_norm; }
+
+#red "Hello world "; yellow "from "; blue "BLUE!!"
+#echo
+
+## -- Functions: ---------------------------------------------
 
 RUN() {
     PRESS "-- $*"
@@ -63,7 +95,7 @@ REAPPLY() {
     RUN unzip -p  files/archive.zip ; echo
 }
 
-## -- Main --------------------------------------------------
+## -- Main: --------------------------------------------------
 
 INIT
 
