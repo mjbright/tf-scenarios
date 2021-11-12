34a4d6eee3f23d0d9987670844d7ec11ab9c881b
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-11-12 11:49:41 +0000

    Renamed file/contents as file1

--- Dependencies/depends_on.local/archive_1file/main.tf
+++ Dependencies/depends_on.local/archive_1file/main.tf
@@ -1,6 +1,6 @@
 
-resource "local_file" "foo" {
-    content     = "foo!"
+resource "local_file" "file1" {
+    content     = "File1 contents !"
     filename = "${path.module}/files/test.txt"
 }
 
@@ -9,7 +9,7 @@ data "archive_file" "init" {
   source_file = "${path.module}/files/test.txt"
   output_path = "${path.module}/files/init.zip"
 
-  depends_on = [ local_file.foo ]
+  depends_on = [ local_file.file1 ]
 }
 
 
