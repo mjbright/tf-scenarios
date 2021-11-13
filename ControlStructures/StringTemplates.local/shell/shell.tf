f99bc0f70ee5bc7d2af64f81474c8a2445fe9ec9
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-11-13 13:01:22 +0000

    Added link to StackOverflow discussion

--- ControlStructures/StringTemplates.local/shell/shell.tf
+++ ControlStructures/StringTemplates.local/shell/shell.tf
@@ -1,6 +1,8 @@
 
 locals {
   # NOTE: Use of $${VAR} in template to have ${VAR} in resulting shell script
+  # Thanks to discussion here:
+  # - https://stackoverflow.com/questions/66953938/escaping-dollar-sign-in-terraform
 
   shell_script = templatefile("${path.module}/templates/run_me.sh.tpl", {
     NUM=10,
