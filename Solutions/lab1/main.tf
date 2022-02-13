dcb9a732eea6f278858247b5bb22f1803430cb84
Michael Bright <1880109+mjbright@users.noreply.github.com>
2022-02-13 17:57:40 +0000

    Updated lab1 solution + added MORE

--- Solutions/lab1/main.tf
+++ Solutions/lab1/main.tf
@@ -1,6 +1,14 @@
 
 provider "aws" {
-    region = "us-west-1"
+  region = "us-west-1"
 }
 
+resource "aws_instance" "example" {
+  ami           = "ami-0e81aa4c57820bb57"
+  instance_type = "t2.micro"
+
+  tags = { LabName = "1.InstallTerraform" }
+}
+
+output  "public_ip"     { value = aws_instance.example.public_ip }
 
