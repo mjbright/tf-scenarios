b5e5a52ec32d99cc20725cff64b3353d0434f5b4
Michael Bright <1880109+mjbright@users.noreply.github.com>
2023-08-23 22:55:46 +0200

    Added -y option for unattended install

--- scripts/DOCKER_install_on_ubuntu.sh
+++ scripts/DOCKER_install_on_ubuntu.sh
@@ -8,7 +8,7 @@ OLD_REMOVE() {
 
 INSTALL_DOCKER() {
    sudo apt-get update
-   sudo apt-get install     ca-certificates     curl     gnupg     lsb-release
+   sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
    sudo mkdir -p /etc/apt/keyrings
    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -17,7 +17,7 @@ INSTALL_DOCKER() {
        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
    sudo apt-get update
-   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 }
 
 
