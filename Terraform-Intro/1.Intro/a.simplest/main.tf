3e5fcc155d7c0b219ac5cf1f17e597a1fe1b7628
Michael Bright <1880109+mjbright@users.noreply.github.com>
2024-07-11 16:24:29 +0000

    Moving skeleton demos for now ...

--- Terraform-Intro/1.Intro/a.simplest/main.tf
+++
@@ -1,9 +0,0 @@
-variable test {
-    description =   "It is good practice to add a description"
-    default     =   "a test value"
-}
-
-output op_test  { value = var.test }
-
-output op_test2 { value = "The same variable '${var.test}' using string interpolation" } 
-
