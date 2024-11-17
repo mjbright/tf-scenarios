b27a892a10bc8fbbbac06d3b3d42ed25387a84e9
Michael Bright <1880109+mjbright@users.noreply.github.com>
2024-11-17 22:03:43 +0000

    Added -text option

--- Solutions/packer-tf-vault/list_images.sh
+++ Solutions/packer-tf-vault/list_images.sh
@@ -1,9 +1,16 @@
 
 # aws ec2 describe-images --owner self --region us-east-1
 
+REGION="us-east-1"
+
+if [ "$1" = "-text" ]; then
+    aws ec2 describe-images --owner self --output text --region $REGION 
+    exit
+fi
+
 mkdir -p ~/tmp/aws
 
-aws ec2 describe-images --owner self --region us-east-1 |
+aws ec2 describe-images --owner self --region $REGION |
         tee ~/tmp/aws/aws.ec2.images.$$.json |
         jq -rc '.Images[] | { ImageId, Name, CreationDate, Tags, Tags }'
 
